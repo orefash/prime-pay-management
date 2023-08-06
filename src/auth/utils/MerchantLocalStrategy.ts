1d33f5f64718c920e3e23d382132f017ef28d163
orefash <orefash@gmail.com>
2023-08-06 19:27:26 +0100

    login fix

--- src/auth/utils/MerchantLocalStrategy.ts
+++ src/auth/utils/MerchantLocalStrategy.ts
@@ -18,7 +18,9 @@ export class MerchantLocalStrategy extends PassportStrategy(Strategy, 'merchant-
 
     async validate(email: string, password: string): Promise<any> {
         const merchant = await this.authService.validateMerchant(email, password);
-        if(!merchant || merchant.isConfirmed){
+
+
+        if(!merchant){
             // throw new UnauthorizedException();
             throw new HttpException("Incorrect Email or Password", HttpStatus.UNAUTHORIZED);
         }
